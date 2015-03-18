1c50cc638d8166e44e9d0a2238681507be68b5fa
dream龙魄 <964123660@qq.com>
2015-03-18 22:02:17 +0800

    迷宫

--- maze/maze/maze.cpp
+++ maze/maze/maze.cpp
@@ -6,25 +6,27 @@
 #include<iostream>
 using namespace std;
 
+#define BLOCKNUM 4//迷宫地图大小
+
 //定义栈的结构
 struct node
 {
 	int x;
 	int y;
-	bool state;//方块是否可以通过
+	int direction;//1代表向右，2代表向下，3代表向左，4代表向上，存放探索方向
 	node *next;
 	node()
 	{
 		next=NULL;
-		state=true;//默认为false，不可通过
 	}
 };
+
 //进栈---插入到head之后
-void Push(node * &head,int x,int y)
+void Push(node * &head,int x,int y,int direction)
 {
 	
      node * temp=new node;
-	 temp->x=x;temp->y=y;
+	 temp->x=x;temp->y=y;temp->direction=direction;
 	 if(head==NULL)
 		 head=temp;
 	 else
@@ -36,55 +38,118 @@ void Push(node * &head,int x,int y)
 }
 
 //出栈--取head后
-node Pop(node * &head)
+void Pop(node * &head)
 {
-	node * ok;node temp;
+	node * ok;
 	if(head!=NULL)
 	{
-		temp.x=head->x;
-		temp.y=head->y;temp.state=head->state;
+		
 		ok=head;
 		head=head->next;
 		delete ok;
 	}
-	return temp;
+	
 	
 }
 
-//查看栈中是否含此元素
-bool GetItem(node *head)
+//获取栈顶元素
+node Getpop(node * head)
 {
-	while(head!=NULL)
-	{
-		
-	}
-	return true;
+	node temp;
+	temp.x=head->x;
+	temp.y=head->y;
+	temp.direction=head->direction;
+	return temp;
 }
+
+//修改栈顶元素方向
+void Change(node * & head,int drection)
+{
+	head->direction=drection;
+}
+
 
 int main()
 {
 	//0表示可以通行
-	int map[4][4]={
+	int map[BLOCKNUM][BLOCKNUM]={
 		{0,0,1,0},
 		{1,0,0,0},
 		{0,0,1,1},
 		{1,0,0,0}
 	};
 	node *head=NULL;//栈的头指针
-	
-	for(int i=0;i<10;i++)
+	Push(head,0,0,1);//入口进栈
+	int i=0;
+	while(i<2)
 	{
-		
-		Push(head,i,i);
+		i++;
+		//对下一块进行探测
+		bool sea=true;
+		while(sea)
+		{
+			node  temp=Getpop(head);
+			cout<<temp.direction<<endl;
+			switch (temp.direction)
+			{
+			case 1:
+				if((temp.x+1)<BLOCKNUM&&map[temp.x+1][temp.y]==0)
+				{
+					Push(head,temp.x+1,temp.y,1);
+					sea=false;
+				}
+				else
+				{
+					Change(head,temp.direction+1);
+				}
+			
+				break;
+			case 2:
+				if((temp.y+1)<BLOCKNUM&&map[temp.x][temp.y+1]==0)
+				{
+					Push(head,temp.x,temp.y+1,1);
+					sea=false;
+				}
+				else
+				{
+					Change(head,temp.direction+1);
+				}
+				break;
+			case 3:
+				if((temp.x-1)>=0&&map[temp.x-1][temp.y]==0)
+				{
+					Push(head,temp.x-1,temp.y,1);
+					sea=false;
+				}
+				else
+				{
+					Change(head,temp.direction+1);
+				}
+				break;
+			case 4:
+				if((temp.y-1)>=0&&map[temp.x][temp.y-1]==0)
+				{
+					Push(head,temp.x-1,temp.y,1);
+					sea=false;
+				}
+				else
+				{
+					Pop(head);
+				}
+				break;
+			default:
+				break;
+			}
+		}
+	}
+	node *kk=head;
+	while(kk!=NULL)
+	{
+		cout<<kk->x<<"  "<<kk->y<<"  "<<kk->direction<<endl;
+		kk=kk->next;
+	}
 
-	}
-	Pop(head);	Pop(head);	Pop(head);	Pop(head);	
-	node * ok=head;
-	while(ok!=NULL)
-	{
-		cout<<"xx: "<<ok->x<<"  yy: "<<ok->y<<endl;
-		ok=ok->next;
-	}
+	
 	return 0;
 }
 
