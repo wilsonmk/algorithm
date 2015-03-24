9341464e6ffc7cf0e5d46e3e242db8ddeeafeef6
dream龙魄 <964123660@qq.com>
2015-03-24 21:43:43 +0800

    二叉树实现

--- Btree/Btree/Btree.cpp
+++ Btree/Btree/Btree.cpp
@@ -20,8 +20,8 @@ struct node
 		lchild=rchild=NULL;
 	}
 };
-//二叉树生成
-void CreateTree(node * & root,string tree)
+//二叉树生成--先序遍历输入要生成的二叉树数据，#代表空结点
+void CreateTree(node * & root)
 {
 	 char data;
 	 cin>>data;
@@ -30,22 +30,17 @@ void CreateTree(node * & root,string tree)
 		 root=new node;
 		 root->data=data;
 		 cout<<"左"<<endl;
-		 CreateTree(root->lchild,tree);
+		 CreateTree(root->lchild);
 		 cout<<"you"<<endl;
-		 CreateTree(root->rchild,tree);
+		 CreateTree(root->rchild);
 	 }
 }
 
 
 int main()
 {
-	string tree;
-	node * root;
-		root=NULL;//根节点
-	//cout<<"请按先序遍历输入要生成的二叉树数据，#代表空结点: "<<endl;	
-	//cin>>tree;
-	CreateTree(root,tree);//生成二叉树
-
+	node * root=NULL;//根节点
+	CreateTree(root);//生成二叉树
 	return 0;
 }
 
